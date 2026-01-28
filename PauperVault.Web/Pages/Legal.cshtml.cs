0a9e82d5f230176456dea77ef82e6affe25eb23a
Jérôme GUILLEVIC <jguillevic@isagri.fr>
2026-01-28 16:32:13 +0100

    Commit initial: PauperVault web + api skeleton

---
+++ PauperVault.Web/Pages/Legal.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PauperVault.Web.Pages
+{
+    public class LegalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
