af9d2668c2086a999f88037a8d1484e56f1a4927
Jérôme GUILLEVIC <jguillevic@isagri.fr>
2026-03-16 17:19:59 +0100

    Modification du style du coût des cartes.

---
+++ PauperVault.Web/Infrastructure/Mana/ManaCostRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PauperVault.Web.Infrastructure.Mana;
+
+public static class ManaCostRenderer
+{
+	private static readonly Regex ManaRegex = new(@"\{([^}]+)\}");
+
+	public static string ToHtml(string? manaCost)
+	{
+		if (string.IsNullOrWhiteSpace(manaCost))
+			return string.Empty;
+
+		return ManaRegex.Replace(manaCost, m =>
+		{
+			var symbol = m.Groups[1].Value.ToLower();
+			return $"<i class=\"ms ms-{symbol}\"></i>";
+		});
+	}
+}
